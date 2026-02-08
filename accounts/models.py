8cfeab95d5b4bce3c21d05197ddac55aae59cca2
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2026-02-08 09:49:29 +0500

    'productModels.py'

--- accounts/models.py
+++ accounts/models.py
@@ -6,5 +6,6 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
 
+
     def __str__(self):
         return self.username
