312ecddd6f3f9bf14fc7a9884443fc4b81b3ed6b
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2026-02-08 09:32:54 +0500

    CUstomUser

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CUSTOM_USER_MODEL = 'accounts.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
--- product/models.py
+++ product/models.py
@@ -1,4 +1,34 @@
 from django.db import models
-
+from accounts.models import CustomUser
 # Create your models here.
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=255)
+#
+#     def __str__(self):
+#         return self.name
+# 
+#
+# class Product(models.Model):
+#
+#     name = models.CharField(max_length=255)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     description = models.TextField()
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#
+#
+#
+#
+#
+# class Comment(models.Model):
+#     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product,on_delete=models.CASCADE)
+#     text = models.TextField()
+#     rating = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return f'{self.user} - {self.product}'
+
+
 
