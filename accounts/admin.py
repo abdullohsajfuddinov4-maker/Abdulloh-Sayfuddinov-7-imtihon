9dba20fb2da32e7cbaf32a405784af0120c7c734
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2026-02-08 19:18:23 +0500

    debug accaunys bifor test api

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Доп. поля", {"fields": ("phone_number", "address")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Доп. поля", {"fields": ("phone_number", "address")}),
+    )
+    list_display = ("username", "email", "phone_number", "is_staff")
--- accounts/views.py
+++ accounts/views.py
@@ -32,31 +32,43 @@ class ProfileView(APIView):
 
 class ProfileUpdateView(APIView):
     permission_classes = [IsAuthenticated]
-    def post(self, request):
-        serializer = ProfileUpdateSerializer(request.user, data=request.data)
+    def patch(self, request):
+        serializer = ProfileUpdateSerializer(instance=request.user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import ResetPasswordSerializer
+
+
 class ResetPasswordView(APIView):
     permission_classes = [IsAuthenticated]
-    def post(self, request):
+
+    def patch(self, request):
         serializer = ResetPasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        if serializer.is_valid():
-            old_password = serializer.validated_data["old_password"]
-            new_password = serializer.validated_data["new_password"]
+        old_password = serializer.validated_data["old_password"]
+        new_password = serializer.validated_data["new_password"]
 
-            if not request.user.check_password(old_password):
-                return Response({"old_password": "eski password mos emas"}, status=status.HTTP_400_BAD_REQUEST)
+        if not request.user.check_password(old_password):
+            return Response(
+                {"old_password": "eski password mos emas"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-            request.user.set_password(new_password)
-            request.user.save()
+        request.user.set_password(new_password)
+        request.user.save(update_fields=["password"])
 
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail": "parol yangilandi"}, status=status.HTTP_200_OK)
+
 
 
 
